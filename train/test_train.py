bfb309fbd197760d61a8a236c1da0a99d50710a8
Alexis92150 <alexis.cacilhas@student-cs.fr>
2024-05-08 15:54:37 +0200

    Update test_train.py

--- train/test_train.py
+++ train/test_train.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class TestTrain(TestCase):
 
-    def test_train(self):
+    def test_clean_data(self):
         df=pd.DataFrame({'survived':[1,0,1],
                          'pclass':[1,2,3],
                          'sex':["male","female","male"],
@@ -14,3 +14,5 @@ class TestTrain(TestCase):
 
         self.assertEqual(result_df.shape[0],3)
         self.assertEqual(result_df.loc["sex"][0],0)
+
+
